27c7441786a76dd10a90c1d05ac34cf982dcdb0b
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-08-23 17:33:01 +0700

    updating FireBase.js with new config

--- src/Firebase/FireBase.js
+++ src/Firebase/FireBase.js
@@ -2,12 +2,19 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   //fill with your firebase config
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGGING_SENDER_ID",
+  appId: "APP_ID"
 };
 
 // Initialize Firebase
